8e95c4c38ac62ddc8f8eb7c9076107282f6747e5
Max <Max@DESKTOP-1LUC6OI>
2022-04-07 14:07:44 +0200

    Add project files.

---
+++ ShiftTracker/Models/ShiftTrackerContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ShiftTracker.Models
+{
+    public class ShiftTrackerContext : DbContext
+    {
+        public DbSet<Shift> Shifts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=localhost;Database=ShiftTrackeDB;Trusted_Connection=True;");
+        }
+    }
+}
